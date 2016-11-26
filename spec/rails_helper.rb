9818a60c101ea8597a2502c58d301f3ba9668064
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2016-11-26 21:00:43 -0200

    chore(tests): Add shoulda-matchers missing configuration
    In order to use shoulda-matchers in RSpec specs
    
    More: https://github.com/thoughtbot/shoulda-matchers#getting-started

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,3 +18,10 @@ RSpec.configure do |config|
   config.include EmailSpec::Matchers
   config.include ActiveSupport::Testing::TimeHelpers
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
