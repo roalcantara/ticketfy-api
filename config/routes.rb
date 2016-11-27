f773aedd1012692ebfc5d764defb31db2e084628
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2016-11-27 19:29:34 -0200

    fix(routes): Set the default format to `json`
    In order to set the access via REST/json standard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'Admin', at: 'admin_auth', skip: [:omniauth_callbacks]
 
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :admins
     end
