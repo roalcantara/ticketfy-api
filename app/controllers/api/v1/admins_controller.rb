662b35689007f66662bc080e29b5b2c4561d7f60
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2016-11-26 23:26:59 -0200

    feat(api): Configure API namespace and version
    In order to control the API version accurately

---
+++ app/controllers/api/v1/admins_controller.rb
@@ -0,0 +1,53 @@
+module Api
+  module V1
+    class AdminsController < ApplicationController
+      before_action :set_admin, only: [:show, :update, :destroy]
+      before_action :authenticate_api_v1_admin!
+
+      def index
+        @admins = Admin.all
+
+        render json: @admins
+      end
+
+      def show
+        render json: @admin
+      end
+
+      def create
+        @admin = Admin.new(admin_attributes)
+        if @admin.save
+          render json: @admin, status: :created
+        else
+          render json: @admin.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @admin.update(admin_attributes)
+          render json: @admin
+        else
+          render json: @admin.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @admin.destroy
+      end
+
+      private
+
+      def set_admin
+        @admin = Admin.find(params[:id])
+      end
+
+      def admin_params
+        params.require(:data).permit(:type, attributes: [:name, :email, :password, :password_confirmation])
+      end
+
+      def admin_attributes
+        admin_params[:attributes] || {}
+      end
+    end
+  end
+end
